8a2a25540c6b17e3f08ec31a47d748a984e6912f
Rodney Wells <rodneywells01@gmail.com>
2018-12-02 18:35:06 -0500

    Day 2 Solution

---
+++ day2.py
@@ -0,0 +1,70 @@
+
+# Part 1
+inventory_ids = ["fonbsmjyqugrapsczckghtvdxl","fonpsmjyquwrnpeczikghtvdxw","fonbsmdymuwrapexzikghtvdxl","fonwsmjyquwrapeczikghttdpl","fonbsmjkquwrapeczjkghtvdxx","yonbsmjyquwrapecgikghtvdxc","donbsmjyquqrapeczikghtadxl","monbsmjyquprgpeczikghtvdxl","fonbsmjyquwvapecqgkghtvdxl","fonbsmjyquwrkphczikghsvdxl","fonbomjyeuwvapeczikghtvdxl","fonwsmjyjuwrapoczikghtvdxl","foybsmjyquwcapeczikghsvdxl","fonbsmjyquwrtaeczikgptvdxl","ponbsmpyquwjapeczikghtvdxl","flnbcmjyquwrqpeczikghtvdxl","fonbsmjyquwrapegzikvbtvdxl","fonbjmjyqgwrazeczikghtvdxl","zoabsmjyquwkapeczikghtvdxl","fonbsmjyquwrapecziktxkvdxl","fonbsxjyrpwrapeczikghtvdxl","fonbsmjbquwqapeciikghtvdxl","lonbsmjyquwraphczikghtvdul","ftnbsmjyquwrapcczikghtxdxl","fonbsmjyqgwrapeczikghtldxc","fonbsmjsquwmapeyzikghtvdxl","fonbsmjyqfwrapecziqghtgdxl","yonbsmjyquwraveczikgftvdxl","fovbsmjyquwrapeczikggkvdxl","fonbsmjyquwrapezzikghbvdvl","fonzsmxyquwrapeczukghtvdxl","fonbemjyquwrapevzikghtvrxl","conbsxjxquwrapeczikghtvdxl","fonbsmjsmewrapeczikghtvdxl","folbsmjyqhwrapqczikghtvdxl","fonbsmjyquwrzneczikghtvdxn","fonbsmjyquirapeczikjhtvdll","fontsmgyquwrgpeczikghtvdxl","fonbsmjyauwrapeczbfghtvdxl","ftnbsmjyquwrapecpifghtvdxl","fonvsmjyqewrapeczikghlvdxl","fonbsljyquwrapecziklhtvdxw","fonbbmjyquwrapeczikghvadxl","ponbsmjyquwrspeczikghivdxl","fonbsmjcquwrapeccikghtvuxl","fonbsmjnquwrapetzikghtvlxl","fonbsmjymuwrapeczieghtvdxr","ffnbsxnyquwrapeczikghtvdxl","fonbsmjytuwrajeczzkghtvdxl","fonssmjyquwhapeczikghkvdxl","fonbsajyuuwrapeczikghlvdxl","fonbsmjyquwrapeczihghtcixl","fohbsmjyquwrapzczirghtvdxl","fonbsmjyquwrapecjqnghtvdxl","fonbsmjytuhrapeczihghtvdxl","foabumjyquwrapeczikghtvdxz","conbsmjyqtwrapeczikggtvdxl","fonbsmjyiqwrapeczokghtvdxl","fondsmjypuwrapeczikghtvjxl","fonbswjyquwrapeczikgvtydxl","fonbsmjyqqwrapeczikkhtvdbl","fonbsmjyquwrapemzitghtvdsl","fonbsmjyquwrspecziegxtvdxl","fonbsmpyquwrgpeczikghtwdxl","fodbsmjqquwrapeczmkghtvdxl","fonbsmjkquwrapeczikghpvdxr","fonbsmjyquwrapeczikshzvmxl","fznbsmjyqulrapeczikghkvdxl","fonbsmjyquwripeczikghtbdjl","fcnbsmjyquzrapecyikghtvdxl","ronbxmjyquwrapeczikghgvdxl","fonbsmuyvuwrgpeczikghtvdxl","fonbsmjyyuwraplczikghtudxl","poxbsmjyqewrapeczikghtvdxl","foabsmjyquwrapecziqghtvpxl","ponbsmjrquwrapeczikchtvdxl","fonzzmjyquwrapeczikghtvdxs","wonbsmjyquwghpeczikghtvdxl","fofbsejyquwrapeczikgctvdxl","ponbsmjyquwrayegzikghtvdxl","fonbumjyquwripeczikghtvdxf","fonbsmqyquwrapeczikgftvdxv","qonbsmjyquwraplczitghtvdxl","fmnbsajdquwrapeczikghtvdxl","fonbsrjyquwrapempikghtvdxl","fonbsmjyquwrapeczikgotudxw","fonbsmtyquwrapeflikghtvdxl","fzqbsmjyquwrapecjikghtvdxl","fdnbsmjyquwraqeclikghtvdxl","fvnbsijyquwrapechikghtvdxl","fovbsmjyquwsapeczikghqvdxl","ffjbsmjyqgwrapeczikghtvdxl","fonbsmjyquwrapeczvkhhivdxl","forbamjjquwrapeczikghtvdxl","fonbwmjyquwtapeyzikghtvdxl","fonvsmjyquwrapeczikglnvdxl","fonnsmjyguwrapeczikghtvxxl","fopbsmjyquwrapeczikghtvaxz","fonbsmjyquwiapeczikrhavdxl","fonbsujyquwrapeczikthtvdjl","fonpsmkyeuwrapeczikghtvdxl","fonbsmjyquwrapeczqkgttvdxk","fonbsmjyqzwrapeczikgrtddxl","fokbsmjiquwrapeczikgltvdxl","fonbsmjyqbwrapeczikghttdxo","fonbsejyquwrapeczikghbvdal","fonblmjyquwyaveczikghtvdxl","fonbsmjyquwlzpepzikghtvdxl","fonbsmjyqulrapbczigghtvdxl","fonbsmjyxuwrapecziyghtvsxl","fonbyjjyquwrapeczikghtvdxn","fonbhmjyquwrapeczikghtjhxl","fonbspjykuwraieczikghtvdxl","aonbsmjyquwwapeczikchtvdxl","fombsmjyquwyapeczikghtvdll","fonbsmjynuwrapeczivgbtvdxl","xonbsmjfquwrapeczikghqvdxl","fonbyzjyquwzapeczikghtvdxl","fbnbsmjyquwrapeczimgvtvdxl","qonbsmjyquwraoeczikgftvdxl","fonbsrjyquwrapeczikghtvjxm","fonbsmjyquwrapxjzykghtvdxl","fonbwgjyquwrapecziklhtvdxl","fonjcmjyouwrapeczikghtvdxl","fonbsmjyquwrapefzisuhtvdxl","fonbsmjyqywrspeczikghtvnxl","qonbsmjyquwrapeczlkuhtvdxl","fonbsmjyqlprapeczikghtvdbl","fonbsmjzquwrapedzikfhtvdxl","fonbsmjyquwrapeczizghtvjxq","fonbsmxyquwrrpeczikghtvcxl","fonpsmjyquwoapeczikghjvdxl","fonbshkyauwrapeczikghtvdxl","fonbsmjysuwrapeczilghpvdxl","fovwsxjyquwrapeczikghtvdxl","fonbsmjyquwrppecnikghmvdxl","fonbkmjyiuwrrpeczikghtvdxl","gonbsmjyquwrapeczikphtudxl","foncsmjyqlwrapeczimghtvdxl","fonbsmjhquwrtpeczikghtvdxg","fogbsmjyquarapeczikghtvdil","fonbsmjyquwraperzekghwvdxl","fonbstjyquwrapeczicghtedxl","fonbsmjoquhrapeczikgotvdxl","fonbsmjykuwrareczikgdtvdxl","fonbsmjyvuwrayeczivghtvdxl","fonbzmgyquwraptczikghtvdxl","fonbsmjyqubrapeczikgftvdxb","fonbgmjyjuwrapeczikghtvdul","fonbsmjzqurrapeczikghtvfxl","fonbsmjyiuwrapeczikgstvtxl","fpnbstjyquwrapeczikghtvdcl","fonbpmjyquwrapeczivghtndxl","fonbsmjyquwrapeczilgptvvxl","fonbsmjyqdwripecbikghtvdxl","fonbsmjytuwgapnczikghtvdxl","fonbsejyquwrapedzikghtvdml","fonbsojyqdwrapeczikghtgdxl","fonbsmjykuwrayeczicghtvdxl","foubsmtyquwrapeczikchtvdxl","fonbqmjyqukrapeyzikghtvdxl","fonbsmjyquwaapenzikghtvdwl","fonbsmeyquwrapeyzixghtvdxl","fonusmjyquhrapeczikgytvdxl","fonbsmjyquwrapwazikqhtvdxl","fonwsmeyquwrapeczikghhvdxl","fonmsmjyquxrspeczikghtvdxl","fonqsmjyqxwrapeczikghtvdml","fonfsmjyquwrapeuzikgatvdxl","fonvsmjyquwrapeczikgrtvdul","fonbsmayquwrapeczikihtvdxm","fonbsmnyquwrapecdifghtvdxl","fonbsmjyeuwraseczikghtvdxo","fonbvvjyquwrapeczikghtvdxi","fonbsmjyquwrapeczbkghtorxl","tonbsmjyqvwrapeczikghtvdcl","fonbsmjyquwrapeczhkgbtvdkl","fonqsmjyquwrapenzibghtvdxl","fontsmeyqudrapeczikghtvdxl","qonbsmjyauwrapeczikghtvdbl","fynbsmjyluwrapeczekghtvdxl","fonbsmjhquwrappczikghtvdxt","conbsmjyquwrapeczikahtvdxz","fonbsmjyquorapeczikvftvdxl","fonbsriyquwrapeczikchtvdxl","yonfsmjyquwrapeczikghtvdxq","fonaomjyquwrapecziwghtvdxl","fonbsxsyqdwrapeczikghtvdxl","fonbsqjyouwrapeczikgltvdxl","fonbstsyquwraleczikghtvdxl","fonbsmjyquwraoecztkghtvdsl","fonbsmjyquwrapezzjkghmvdxl","fonbwmjyqnwrapecpikghtvdxl","fonbsmvyqbwrapeczikghtvdsl","fonbsijyquwrazeczikghtvdwl","fonbsmjyouwrapewzikghtldxl","xonbsmjyqcwrapeczikghtvdul","fonbgmjxquwrajeczikghtvdxl","fokbsmjyquwrapechikghtrdxl","fonbqmjyqawrapeczikghtrdxl","fonbwmjzquwtapeyzikghtvdxl","fonbsmjyquwrapecdikgatvdnl","fonbsmjyqowrkpeczikghtvdxj","fonbsmjyquwkapejzikuhtvdxl","fonbsmjyquwrabeozikghtmdxl","fonbsijyeuwrapeczikghtvdxh","fonbsmjhquprapeczizghtvdxl","fonesmjyquwrapcczikghtvdxh","fonbamjyquwrapeczifrhtvdxl","foabsmjyquwpapeczikghtvdxs","fonbsmjyquwrapeczukghivdxh","fonbsejyoulrapeczikghtvdxl","fonbsmjyquwraceczikgdmvdxl","eonbsmjyquerppeczikghtvdxl","ffnzsmjyquwgapeczikghtvdxl","donbsmyyquwrapeczirghtvdxl","fjnbsmjyqufrapeczikghtwdxl","fonfsmjyquwrareczigghtvdxl","fonusmjyquwrapeczikgetvexl","tonbsmjyqpwrapeczikghtjdxl","fonbsmjhqukkapeczikghtvdxl","fonbsmjyqusraseczikghtvzxl","fonbsmjyquygapeczxkghtvdxl","folbsmjyquwraqeczikghjvdxl","fonbsmjyquwrppecjinghtvdxl","fonbsmjyquwraepczhkghtvdxl","fonbfmjyquwrapeczisghtrdxl","fsnbsmjwqubrapeczikghtvdxl","fonbspjyquwrapjczikghtedxl","fowbsmjyquwrapeczikghtbdbl","fonbymjyquwrapeczikghlvdrl","fonbsmjyruwrapecbikghtvixl","fonyqmjyqufrapeczikghtvdxl","focbscjyquwrapeczmkghtvdxl","fonbsmjyqtwnkpeczikghtvdxl","eonbsmjyquwrameczizghtvdxl","zonbsmjyqcwrapeczikghtvhxl","foubsmjyquwrapehzikghtvnxl","ffnbsmjyquwrapetzikghtjdxl","fonbjgjyquwrapkczikghtvdxl","fonbwmjyquwqapeczdkghtvdxl","forbsmjyquwrapeczikkhtvdml","fonbsmjyiuwrapeczivghevdxl","fonbsmjyquwrapeglikghwvdxl","fopgsmjyquwrapegzikghtvdxl","fonbsmjyqzwrajeczikghtldxl","fonbsmjyruwrapexzmkghtvdxl","fonbsmjyquwrdpeczikxstvdxl","fonbsmjyquwrapeezivghtvdql","fonbdmjyqujsapeczikghtvdxl"]
+total_two_count = 0 
+total_three_count = 0 
+
+# inventory_ids = [inventory_ids[0]]
+for id in inventory_ids: 
+	id_lookup = {}
+	for char in id: 
+		if char in id_lookup: 
+			id_lookup[char] += 1 
+		else: 
+			id_lookup[char] = 1  
+
+	two_count = False 
+	three_count = False 
+	for char in id_lookup: 
+		two_count = two_count or id_lookup[char] == 2
+		three_count = three_count or id_lookup[char] == 3 
+
+	if two_count: total_two_count += 1  
+	if three_count: total_three_count += 1  
+
+checksum = total_three_count * total_two_count
+print(f"Checksum: {checksum}")
+print(f"total_two_count: {total_two_count}")
+print(f"total_three_count: {total_three_count}")
+
+
+# Part 2 
+"""
+for each id:
+	valid_matches = []  
+	for each id2: 
+		for idx in range(len(id))
+"""
+
+correct_answer = None
+for idx in range(len(inventory_ids)): 
+	for idx2 in range(len(inventory_ids)): 
+		# Skip identical ids 
+		if idx == idx2: 
+			continue 
+		 
+		# Evaluate the two ids for a match. 
+		error = False
+		valid = True 
+		for char_idx in range(len(inventory_ids[idx])): 
+			if inventory_ids[idx][char_idx] != inventory_ids[idx2][char_idx]: 
+				if error == True:
+					valid = False
+					break
+				error = True 
+
+		# Break if valid
+		if valid:
+			correct_answer = [idx, idx2]
+			break
+
+	# Break if correct_answer found
+	if correct_answer != None: 
+		break
+
+if correct_answer != None: 
+	print(inventory_ids[correct_answer[0]])
+	print(inventory_ids[correct_answer[1]])
+else: 
+	raise Exception("correct_answer was not found")
+
